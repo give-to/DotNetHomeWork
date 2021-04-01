ed363873800a5b06657301ea5281ac1ddf97c9d1
give-to <1139019079@qq.com>
2021-04-01 22:15:09 +0800

    HomeWork5

---
+++ HomeWork5/HomeWork5/Order.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    class Order
+    {
+        private static int count = 0;
+
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public DateTime PayTime { get; }
+        private double CostSum { get; set; }
+        public string SenderAddress { get; set; }
+        public string ReceiverAddress { get; set; }
+        public int ID { get; }
+        public List<OrderDetails> Goods;
+
+        public Order(string sender,string receiver,string senderAddress,string receiveAddress, List<OrderDetails> goods)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            SenderAddress = senderAddress;
+            ReceiverAddress = receiveAddress;
+            PayTime = DateTime.Now;
+            count++;
+            ID = count;
+            Goods = goods;
+            foreach(OrderDetails orderDetails in goods)
+            {
+                CostSum += orderDetails.CostSum;
+            }
+        }
+        
+        public double GetRefreshCostSum()
+        {
+            CostSum = 0;
+            foreach (OrderDetails orderDetails in Goods)
+            {
+                CostSum += orderDetails.CostSum;
+            }
+            return CostSum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("订单ID：" +ID);
+            str.Append('\n');
+            str.Append("下单时间：" + PayTime + '\n');
+            str.Append("用户：" + Sender + "-------->" + Receiver+'\n');
+            str.Append("地址：" + SenderAddress + "-------->" + ReceiverAddress+ '\n');
+            str.Append("订单明细：" + '\n');
+            foreach (OrderDetails orderDetails in Goods)
+            {
+                str.Append(orderDetails.ToString());
+            }
+            str.Append("总价为：" + GetRefreshCostSum() + '\n');
+            str.Append('\n');
+            return str.ToString();                
+        }
+        
+    }
+}
---
+++ HomeWork5/HomeWork5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService orderService = new OrderService();
+            int num = 3;//订单总数
+            //添加订单
+            for(int i=0;i<num;i++)
+            {
+                orderService.AddOneOrder();
+            }
+            //删除订单
+            Console.Clear();
+            orderService.DeleteOneOrder();
+            Console.Clear();
+            orderService.DisplayOrders();
+
+            //修改订单
+            Console.Clear();
+            orderService.ModifyOrder();
+            Console.Clear();
+            orderService.DisplayOrders();
+
+            //查询订单
+            Console.Clear();
+            orderService.SearchOrder();
+        }
+    }
+}
